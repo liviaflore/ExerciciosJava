4b159af455ebbbfdbd80388542cbcd438bf3b8f4
Lívia F Lo Re <106773988+liviaflore@users.noreply.github.com>
2022-07-08 16:23:09 -0300

    Lista do dia 08/07/2022

--- Exercicio2.java
+++ Exercicio2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Exercicio2 {
 
 	public static void main(String[] args) {
-		// Faça um programa que entre com três números e coloque em ordem crescente.
+		// Faça um programa que entre com três números e coloque em ordem *decrescente.
 		
 		int x, y, z;
 	    
